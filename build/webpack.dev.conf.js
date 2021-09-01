bbcbf2e5a443ac9b92956ec9a1f6dfd20b1c9fd6
Alex Vlasov <drumlife182@gmail.com>
2021-09-01 21:22:13 +0300

    inital project && finish desktop header && promo section

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,29 @@
+/* Development config: */
+const webpack = require('webpack')
+
+const { merge } = require('webpack-merge')
+// Base config
+const baseWebpackConfig = require('./webpack.base.conf')
+
+const devWebpackConfig = merge(baseWebpackConfig, {
+  mode: 'development',
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: baseWebpackConfig.externals.paths.dist,
+    host: '0.0.0.0',
+    port: 8081,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  },
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      filename: '[file].map'
+    })
+  ]
+})
+
+module.exports = new Promise((resolve, reject) => {
+  resolve(devWebpackConfig)
+})
